7153b2a59f54cb17037b4761ef24c8d61ca4cebc
Pradeeshkumar-k <pradeeshkumark600@gmail.com>
2025-11-24 16:55:40 +0530

    before angular upgrade

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+
+  BASE_SAS_URL: import.meta.env['BASE_SAS_URL'],
+  recaptchaSiteKey: import.meta.env['RECAPTCHA_KEY'],
+  API_URL: import.meta.env['API_URL']
+};

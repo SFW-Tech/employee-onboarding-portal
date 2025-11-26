687e46220e6f3da50646db1f1aa40ce144c205ae
Pradeeshkumar-k <pradeeshkumark600@gmail.com>
2025-11-26 14:24:40 +0530

    Remove invalid Vite env and webpack files from Angular project

--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-
-  BASE_SAS_URL: import.meta.env['BASE_SAS_URL'],
-  recaptchaSiteKey: import.meta.env['RECAPTCHA_KEY'],
-  API_URL: import.meta.env['API_URL']
-};

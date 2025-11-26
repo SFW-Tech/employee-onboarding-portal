a1889a8e936a625656a97952439ad083a4096e40
Pradeeshkumar-k <pradeeshkumark600@gmail.com>
2025-11-26 15:57:30 +0530

    Fix Dockerfile path to dist/browser and update deployment config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  BASE_SAS_URL:
+    "https://sfwalpha.blob.core.windows.net/sfwalphafiles/dummy.pdf?sv=2024-11-04&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2025-12-31T01:07:53Z&st=2025-11-17T16:52:53Z&spr=https&sig=lFLoYjrz8UHuj1DoNStHQmij4EBmIiD2/SDPHMP9b8o=",
+  recaptchaSiteKey: "6LdGawssAAAAAJdfBSyQc9EShLEp3N-uGbCxY04d",
+  API_URL:
+    "https://azure-proxy-production.up.railway.app/api/proxy/dynamics?endPoint=cr276_sfemployeemasters",
+  production: true,
+};
